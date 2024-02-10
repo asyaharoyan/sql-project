7d619948e74a14917fbe15d38c6fe940e293f3ac
Asya Haroyan <alharoyan@gmail.com>
2024-02-10 11:09:45 +0000

    Add requirements.txt

--- sql_crud.py
+++ sql_crud.py
@@ -38,7 +38,50 @@ ada_lovelace = Programmer(
     famous_for = "First Programmer"
 )
 
+alan_turing = Programmer(
+    first_name="Alan",
+    last_name="Turing",
+    gender="M",
+    nationality="British",
+    famous_for="Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name="Grace",
+    last_name="Hopper",
+    gender="F",
+    nationality="American",
+    famous_for="COBOL language"
+)
+
+margaret_hamilton = Programmer(
+    first_name="Margaret",
+    last_name="Hamilton",
+    gender="F",
+    nationality="American",
+    famous_for="Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name="Bill",
+    last_name="Gates",
+    gender="M",
+    nationality="American",
+    famous_for="Microsoft"
+)
+
+tim_berners_lee = Programmer(
+    first_name="Tim",
+    last_name="Berners-Lee",
+    gender="M",
+    nationality="British",
+    famous_for="World Wide Web"
+)
+
 # add each instance of our programmers to our session
+# session.add(ada_lovelace)
+session.add(ada_lovelace)
+session.add(ada_lovelace)
 session.add(ada_lovelace)
 
 # commit our session to the database
